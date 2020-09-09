bfde415d1331023aeec05b642ee8be8c9bd9efed
Anton Lobanov <lobanovvv@mail.ru>
2020-09-09 16:31:26 +0300

    work version

--- topNav_clickOnTitle.py
+++ topNav_clickOnTitle.py
@@ -1,14 +1,30 @@
+# Проверка переходов по клику на заголовки в верхнем меню
+# Структура теста:
+#   - клик на заголовок
+#   - проверка url
+
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-#from selenium.webdriver.remote.webelement import WebElement
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.set_window_size(1920,1080)
 driver.get("http://prodpo.ru")
 
-element = driver.find_elements_by_css_selector("ul.menu-top > li > a > span")
-element.click()
-assert driver.current_url == "https://prodpo.ru/obuchenije-po-professijam.html"
+link_url = [
+"https://prodpo.ru/obuchenije-po-professijam.html",
+"https://prodpo.ru/obuchenie-po-okhrane-truda.html",
+"https://prodpo.ru/bezopasnyye-raboty-na-vysote.html",
+"https://prodpo.ru/obuchenie-po-pozharnoi-bezopasnosti.html",
+"https://prodpo.ru/instruktazhi-i-obuchenie-po-elektrobezopasnosti-v-moskve.html",
+"https://prodpo.ru/obuchenie-po-promyshlennoj-bezopasnosti.html",
+"https://prodpo.ru/kontakty.html"
+]
+
+i = 0
+while i < len(link_url):
+    element = driver.find_elements_by_css_selector("ul.menu-top > li > a > span")
+    element[i].click()
+    assert driver.current_url == link_url[i], "url fail"
+    i += 1
 
 driver.quit()
